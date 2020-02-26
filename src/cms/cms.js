9b93262c9d6ad679c5d1d1f81799a2fe10307c16
lulu70 <liorco70@yahoo.com>
2020-02-26 18:51:04 +0100

    10

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,5 @@
 import CMS from "netlify-cms-app"
+import React from "react"
 
 CMS.registerEditorComponent({
   // Internal id of the component
@@ -8,24 +9,23 @@ CMS.registerEditorComponent({
   // Fields the user need to fill out when adding an instance of the component
   fields: [
     { name: "src", label: "Image", widget: "image" },
-    { name: "alt", label: "Alt text", widget: "string" },
     { name: "title", label: "Title", widget: "string" },
   ],
   // Pattern to identify a block as being an instance of this component
   pattern: /^mdxImage (\S+)$/,
   // Function to extract data elements from the regexp match
-  //   fromBlock: function(match) {
-  //     return {
-  //       id: match[1],
-  //     }
-  //   },
+  fromBlock: function(match) {
+    return {
+      alt: match[1],
+    }
+  },
   // Function to create a text block from an instance of this component
   toBlock: function(obj) {
     return `<Image fileName="${obj.title}" />`
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
-  //   toPreview: function(obj) {
-  //     return `<pre>{JSON.stringify(${obj}, null, 2)}</pre>`
-  //   },
+  toPreview: function(obj) {
+    return <div style={{ color: "red" }}>To preview</div>
+  },
 })
