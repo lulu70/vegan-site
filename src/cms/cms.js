09e8de8d4b6e6e2946de7d1f0829710ea0d14dc2
lulu70 <liorco70@yahoo.com>
2020-02-27 17:32:36 +0100

    25

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,33 +1,5 @@
 import CMS from "netlify-cms-app"
-
-CMS.registerEditorComponent({
-  // Internal id of the component
-  id: "mdxImage",
-  // Visible label
-  label: "MDXImage",
-  // Fields the user need to fill out when adding an instance of the component
-  fields: [{ name: "src", label: "Image", widget: "image" }],
-  // Pattern to identify a block as being an instance of this component
-  // pattern: /<Image fileName=".+" \/>/,
-  //   // Function to extract data elements from the regexp match
-  // fromBlock: function(match) {
-  //   return {
-  //     match: match[0],
-  //   }
-  // },
-  // Function to create a text block from an instance of this component
-  toBlock: function(obj) {
-    const fileName = obj.src
-      ? obj.src.replace("../assets/images/", "").split(".")[0]
-      : ""
-    return `<Image fileName="${fileName}" />`
-  },
-  // Preview output for this component. Can either be a string or a React component
-  // (component gives better render performance)
-  // toPreview: ({ match }) => {
-  //   const fileName = match.replace('<Image fileName="', "").split('"')[0]
-  //   const src = "../assets/images/" + fileName + ".jpg"
-  //   console.log(src)
-  //   return "photo"
-  // },
-})
+import mdxImageWidget from "./mdxImageWidget"
+import BlogPostPreview from "./BlogPostPreview"
+CMS.registerEditorComponent(mdxImageWidget)
+CMS.registerPreviewTemplate("blog", BlogPostPreview)
