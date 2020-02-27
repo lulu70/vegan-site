5b0e098deb93542bf417b4547c7f5d69819d8191
lulu70 <liorco70@yahoo.com>
2020-02-27 23:47:20 +0100

    13

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,3 @@
 import CMS from "netlify-cms-app"
 import mdxImageWidget from "./mdxImageWidget"
-import BlogPostPreview from "./BlogPostPreview"
 CMS.registerEditorComponent(mdxImageWidget)
-CMS.registerPreviewTemplate("blog", BlogPostPreview)
