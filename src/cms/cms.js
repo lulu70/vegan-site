a05f5e480165240b6eb187c28eca27ba5c75d418
lulu70 <liorco70@yahoo.com>
2020-03-16 14:00:45 +0100

    add: added preview template to recipes...

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,5 @@ import CMS from "netlify-cms-app"
 import BlogPreview from "./BlogPreview"
 import stylesheet from "./styles.css"
 CMS.registerPreviewTemplate("blog", BlogPreview)
+CMS.registerPreviewTemplate("recipe", BlogPreview)
 CMS.registerPreviewStyle(stylesheet)
