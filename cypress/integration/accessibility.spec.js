81ce5e050ddeda8d4b13355e7f33d9f72eb833bf
lulu70 <liorco70@yahoo.com>
2020-05-23 11:51:22 +0200

    added accessibility tests

---
+++ cypress/integration/accessibility.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+describe("Accessibility tests", () => {
+  const checkAccessibilityOnPage = ({ path, name, only }) => {
+    const injectBody = () => {
+      cy.visit(path)
+        .get("main")
+        .injectAxe()
+        .wait(500)
+        .checkA11y()
+    }
+    if (only) {
+      it.only(`Has no detectable accessibility violations on ${name} page`, () => {
+        injectBody()
+      })
+    } else {
+      it(`Has no detectable accessibility violations on ${name} page`, () => {
+        injectBody()
+      })
+    }
+  }
+  checkAccessibilityOnPage({ path: "/", name: "home" })
+  checkAccessibilityOnPage({ path: "/blog", name: "blog" })
+  checkAccessibilityOnPage({ path: "/recipes", name: "recipes" })
+  checkAccessibilityOnPage({
+    path: "/recipes/high-protein-vegan-burgers",
+    name: "a recipe page",
+  })
+  checkAccessibilityOnPage({
+    path: "/the-vegan-pantry",
+    name: "the vegan pantry",
+  })
+})
